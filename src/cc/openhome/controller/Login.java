275af5442229507871e8b22310a35fc787f4992a
ybfqlt <823030735@qq.com>
2019-03-05 20:03:16 +0800

    day1

---
+++ src/cc/openhome/controller/Login.java
@@ -0,0 +1,41 @@
+package cc.openhome.controller;
+
+import java.io.*;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/login.do")
+public class Login extends HttpServlet {
+    private final String USERS = "/home/ltt/IdeaProjects/Gossip/users";
+    private final String SUCCESS_VIEW = "member.view";
+    private final String ERROR_VIEW="index.html";
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        if(checkLogin(username,password)){
+            request.getRequestDispatcher(SUCCESS_VIEW).forward(request,response);
+        }
+        else{
+            response.sendRedirect(ERROR_VIEW);
+        }
+    }
+    private boolean checkLogin(String username,String password)throws IOException{
+        if(username!=null&&password!=null){
+            for(String file:new File(USERS).list()){
+                if(file.equals(username)){
+                    BufferedReader reader = new BufferedReader(new FileReader(USERS+"/"+file+"/profile"));
+                    String passwd = reader.readLine().split("\t")[1];
+                    if(passwd.equals(password)){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
---
+++ src/cc/openhome/controller/Register.java
@@ -0,0 +1,67 @@
+package cc.openhome.controller;
+
+import java.io.*;
+import java.util.*;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/register.do")
+public class Register extends HttpServlet {
+    private final String USERS = "/home/ltt/IdeaProjects/Gossip/users";
+    private final String SUCCESS_VIEW = "success.view";
+    private final String ERROR_VIEW = "error.view";
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String email = request.getParameter("email");
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        String confirmedPasswd = request.getParameter("confirmedPasswd");
+        List<String> errors = new ArrayList<String>();
+        if(isInvalidEmail(email)) {
+            errors.add("未填写邮件或邮件格式不正确");
+        }
+        if(isInvalidUsername(username)) {
+            errors.add("用户名称为空或已存在");
+        }
+        if(isInvalidPassword(password,confirmedPasswd)) {
+            errors.add("请确认密码符合格式并再次确认密码");
+        }
+        String resultPage = ERROR_VIEW;
+        if(!errors.isEmpty()) {
+            request.setAttribute("errors", errors);
+        }
+        else {
+            resultPage = SUCCESS_VIEW;
+            createUserData(email, username, password);
+        }
+        request.getRequestDispatcher(resultPage).forward(request,response);
+    }
+    private boolean isInvalidEmail(String email) {
+        return email == null || !email.matches("^[_a-z0-9-]+([.]" + "[_a-z0-9-]+)*@[a-z0-9-]+([.][a-z0-9-]+)*$");
+    }
+    private boolean isInvalidUsername(String username) {
+        for (String file : new File(USERS).list()) {
+            if (file.equals(username)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    private boolean isInvalidPassword(String password,String confirmedPasswd) {
+        return password == null || password.length() < 6 || password.length() > 16 || !password.equals(confirmedPasswd);
+    }
+    private void createUserData(String email,String username,String password) throws IOException {
+        File userhome = new File(USERS + "/" + username);
+        userhome.mkdir();
+        BufferedWriter writer = new BufferedWriter(new FileWriter(userhome + "/profile"));
+        writer.write(email + "\t" + password);
+        writer.close();
+    }
+   /* protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    }*/
+}
